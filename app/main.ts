d8de488d68d086196299dac98d341cf1695c790f
Shobhit Dixit <shobhit-m@github.com>
2016-05-09 15:23:43 +0530

    header component

--- app/main.ts
+++ app/main.ts
@@ -1,6 +1,4 @@
 import { bootstrap }    from '@angular/platform-browser-dynamic';
 import { ContainerComponent } from './components/container.component';
-import { ROUTER_PROVIDERS } from '@angular/router-deprecated';
 
-
-bootstrap(ContainerComponent, [ROUTER_PROVIDERS]);
+bootstrap(ContainerComponent);
--- js-map/main.js
+++ js-map/main.js
@@ -1,6 +1,5 @@
 "use strict";
 var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
 var container_component_1 = require('./components/container.component');
-var router_deprecated_1 = require('@angular/router-deprecated');
-platform_browser_dynamic_1.bootstrap(container_component_1.ContainerComponent, [router_deprecated_1.ROUTER_PROVIDERS]);
+platform_browser_dynamic_1.bootstrap(container_component_1.ContainerComponent);
 //# sourceMappingURL=main.js.map
