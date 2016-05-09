d5fe047b30042fd3895de4ce660bc4bcbbce6781
Shobhit Dixit <shobhit-m@github.com>
2016-05-09 17:28:02 +0530

    mock list

--- js-map/services/contact-list.service.js
+++ js-map/services/contact-list.service.js
@@ -9,19 +9,23 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 var core_1 = require("@angular/core");
+var mock_contact_list_1 = require("../mock-contact-list");
 var ContactListService = (function () {
     function ContactListService() {
-        this._contactList = [
-            { id: 1, firstName: "Kishan", lastName: "Yadav", email: "kishan.yadav@metacube.com", mobNumber: "9414234126" },
-            { id: 2, firstName: "Ram", lastName: "Sharma", email: "ram.sharma@metacube.com", mobNumber: "9432456781" },
-            { id: 3, firstName: "Lakshman", lastName: "Singh", email: "lakshman.singh@metacube.com", mobNumber: "9876567856" },
-            { id: 4, firstName: "Bharat", lastName: "Khan", email: "bharat.khan@metacube.com", mobNumber: "9878612345" },
-            { id: 5, firstName: "Lucy", lastName: "Doe", email: "lucy.doe@metacube.com", mobNumber: "9786533212" },
-            { id: 6, firstName: "Jack", lastName: "Reacher", email: "jack.reacher@metacube.com", mobNumber: "9007812345" }
-        ];
     }
     ContactListService.prototype.getContactList = function () {
-        return this._contactList;
+        return mock_contact_list_1.CONTACTLIST;
+    };
+    ContactListService.prototype.getContact = function (id) {
+        return mock_contact_list_1.CONTACTLIST.find(function (item) { return item.id === id; });
+    };
+    ContactListService.prototype.saveContact = function (contact) {
+        contact.id = new Date().getTime();
+        mock_contact_list_1.CONTACTLIST.push(contact);
+    };
+    ContactListService.prototype.updateContact = function (contact) {
+        this._contact = this.getContact(contact.id);
+        this._contact = contact;
     };
     ContactListService = __decorate([
         core_1.Injectable(), 
