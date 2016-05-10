3951ecafb5b3289ff3236297b78b65b5b0a439f3
Shobhit Dixit <shobhit-m@github.com>
2016-05-10 12:57:18 +0530

    pipes

--- app/components/contact-list.component.ts
+++ app/components/contact-list.component.ts
@@ -3,17 +3,20 @@ import { Component } from "@angular/core";
 import { Contact } from "../models/contact";
 import { AutoGrowDirective } from "../directives/autogrow.directive";
 import { ContactListService } from "../services/contact-list.service";
+import { SearchPipe } from "../pipes/search.pipe";
 
 @Component({
   selector: 'contact-list',
   templateUrl: "app/templates/contact-list.tpl.html",
   styles: ['.search-box { width:180px }'],
   directives: [AutoGrowDirective],
-  providers: [ContactListService]
+  providers: [ContactListService],
+  pipes: [SearchPipe]
 })
 
 export class ContactListComponent {
   contactList: Contact[];
+  searchText: string = "";
 
   constructor(private _contactListService: ContactListService){
     this.contactList = _contactListService.getContactList();
