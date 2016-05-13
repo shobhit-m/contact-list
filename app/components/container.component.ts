68ddda1a3df34da1b64b83cfe2fc127146a976ad
shobhit <shobhit.dixit@metacube.com>
2016-05-14 05:02:18 +0530

    changes

--- app/components/container.component.ts
+++ app/components/container.component.ts
@@ -8,20 +8,7 @@ import {Component} from "@angular/core";
 export class ContainerComponent {
   // properties
   // title = "Shobhit";
-  // imagePath = "https://angular.io/resources/images/logos/standard/shield-large.png";
 
 
   // methods
-  // onClick(){
-  //   alert("button clicked");
-  // }
 }
-
-
-// <img [src]="imagePath" />
-// <img bind-src="imagePath" />
-
-// <button (click)="onClick()">Click Me</button>
-// <button on-click="onClick()">Click Me</button>
-//
-// <input [(ngModel)]="title" type="text"/>
